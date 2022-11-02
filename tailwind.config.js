865d9584d4f8e2887425d175af42a3519119baf7
DianneFabiano <dfabiano@gbox.adnu.edu.ph>
2022-11-02 23:07:45 +0800

    tailwind added
    hello tailwind(?)

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/*.html',
+    './*.py'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'default': "url('/static/img/sky_bg.jpg')",
+      },
+      fontFamily: {
+        baloo: [
+          'Baloo 2',
+          'cursive'
+        ]
+      },
+      fontSize: {
+      },
+    },
+    colors: {
+      blue_txtbox: '#E2F1FF',
+      blue_btn: '#3EB3F5',
+      blue_hover: '#229DE2',
+      blue_bg: '#9CDEF6',
+      gold: '#FFD700',
+      orange: '#FFA800',
+      gray: '#363636',
+    },
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      semibold: 600,
+
+    },
+    borderRadius: {
+      'large': '20px'
+    }
+  },
+  plugins: [],
+}
