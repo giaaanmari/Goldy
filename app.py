c3a55156386e15c9c473e6c660f40b806a216471
Please don't call me Mari <52728098+giaaanmari@users.noreply.github.com>
2022-11-25 13:57:42 +0800

    train.py added
    separated train function from flask.py

--- app.py
+++ app.py
@@ -1,98 +1,16 @@
-from flask import Flask, render_template
-from nltk.stem import WordNetLemmatizer
-from requests import request
-lemmatizer = WordNetLemmatizer()
 import nltk
-import json
+nltk.download('popular')
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 import pickle
-
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
-import random
-
-
-words = []
-classes = []
-documents = []
-ignore_words = ['?', '!']
-
-data = open('intents.json').read()
-intents = json.loads(data)
-
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-        # tokenization
-        word_token = nltk.word_tokenize(pattern)
-        words.extend(word_token)
-        
-        documents.append((word_token, intent['tag']))
-        
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
-words = [lemmatizer.lemmatize(word_token.lower()) 
-         for word_token in words if word_token not in ignore_words]
-words = sorted(list(set(words)))
-    
-classes = sorted(list(set(classes)))
-
-print(len(documents), "documents")
-
-print(len(classes), "classes", classes)
-
-print(len(words), "unique sets of words", words)
-
-pickle.dump(words,open('words.pkl','wb'))
-pickle.dump(classes,open('classes.pkl','wb'))
-
-train = []
-
-output_empty = [0] * len(classes)
-
-for doc in documents:
-    bag = []
-    pattern_words = doc[0]
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    
-    for word_token in words:
-        bag.append(1) if word_token in pattern_words else bag.append(0)
-        
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-    train.append([bag, output_row])
-    train.append([bag,output_row])
-    
-random.shuffle(train)
-train = np.array(train)
-
-train_x = list(train[:,0])
-train_y = list(train[:,1])
-print("Training data created")
-
-model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation='softmax'))
-
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-#fitting and saving the model 
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.h5', hist)
-print("model created")
-
 from keras.models import load_model
-model = load_model('chatbot_model.h5')
+model = load_model('model.h5')
+import json
+import random
 intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
-
+words = pickle.load(open('texts.pkl','rb'))
+classes = pickle.load(open('labels.pkl','rb'))
 
 def clean_up_sentence(sentence):
     # tokenize the pattern - split words into array
@@ -100,22 +18,22 @@ def clean_up_sentence(sentence):
     # stem each word - create short form for word
     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
     return sentence_words
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
 def bow(sentence, words, show_details=True):
     # tokenize the pattern
     sentence_words = clean_up_sentence(sentence)
     # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)
-    
+    bag = [0]*len(words)  
     for s in sentence_words:
-            for i, w in enumerate(words):
-                if w == s: 
-                    # assign 1 if current word is in the vocabulary position
-                    bag[i] = 1
-                    if show_details:
-                        print ("found in bag: %s" % w)
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
     return(np.array(bag))
+
 
 def predict_class(sentence, model):
     # filter out predictions below a threshold
@@ -126,23 +44,10 @@ def predict_class(sentence, model):
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
-    
     for r in results:
-            return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
     return return_list
 
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/chat')
-def chat():
-    return render_template('chatbox.html')
-    
-@app.route('/get')
 def getResponse(ints, intents_json):
     tag = ints[0]['intent']
     list_of_intents = intents_json['intents']
@@ -152,9 +57,24 @@ def getResponse(ints, intents_json):
             break
     return result
 
-def chatbot_response(text):
-    ints = predict_class(text, model)
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
     res = getResponse(ints, intents)
     return res
+
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+app.static_folder = 'static'
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route("/chat")
+def chat():
+    return render_template("chatbox.html")
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return chatbot_response(userText)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
