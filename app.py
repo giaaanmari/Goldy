bbd57d3c9861b75caa90a22f0233e6c3c0755554
DianneFabiano <dfabiano@gbox.adnu.edu.ph>
2023-04-16 21:11:01 +0800

    get definition function
    print the definition of the input that is not available in the dataset

--- app.py
+++ app.py
@@ -12,9 +12,15 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import words
 from nltk.corpus import stopwords
+from nltk.corpus import wordnet
 lemmatizer = WordNetLemmatizer()
 import pickle
 import enchant
+<<<<<<< Updated upstream
+=======
+import re
+import string
+>>>>>>> Stashed changes
 import numpy as np
 from keras.models import load_model
 model = load_model('model.h5')
@@ -22,6 +28,15 @@ import json
 import random
 import re
 
+<<<<<<< Updated upstream
+=======
+nltk.download('popular')
+nltk.download('words')
+nltk.download('wordnet')
+nltk.download('punkt')
+nltk.download('stopwords')
+
+>>>>>>> Stashed changes
 intents = json.loads(open('intents.json', encoding='utf-8').read())
 words = pickle.load(open('texts.pkl','rb'))
 tags = pickle.load(open('tags.pkl','rb'))
@@ -34,7 +49,9 @@ for word in words:
     dictionary.add(word)
 default_responses = ["I'm sorry, I didn't quite understand what you said. Can you please try asking me again in a different way?",
                      "I'm sorry, I don't have the answer to that question right now. But don't worry, I'll keep learning and hopefully, I'll be able to help you with your question soon.",
-                     "Hmm, I'm not quite sure what you're asking. Can you please give me more information or context about your question?"]
+                     "Hmm, I'm not quite sure what you're asking. Can you please give me more information or context about your question?",
+                     "I'm sorry, but I'm not sure what you're trying to say. Can you please provide me with more information or a specific question so I can better assist you?",
+                     "I'm having trouble understanding what you're trying to communicate. Can you please provide me with more context or a specific topic so I can better assist you?"]
 #### PRE-PROCCESSING
 
 def clean_up_sentence(sentence):
@@ -73,16 +90,25 @@ def predict_class(sentence, model):
         return_list.append({"intent": tags[r[0]], "probability": str(r[1])})
     return return_list
 
-def getResponse(ints, intents_json):
+def getResponse(ints, intents_json, msg):
     global default_responses
     global context
     global current_context
     global previous_context
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     if len(ints) > 0:
         tag = ints[0]['intent']
     else:
         tag = None 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     list_of_intents = intents_json['intents']
+    
     for i in list_of_intents:
         if tag == i['tag']:
             possible_responses = i['responses']                   
@@ -93,12 +119,57 @@ def getResponse(ints, intents_json):
             else:
                 previous_context = current_context
                 current_context = i['context']
+<<<<<<< Updated upstream
                 return random.choice(possible_responses)        
     return random.choice(default_responses)
 
 def spellcheck(msg):
     corrected_words = " ".join([dictionary.suggest(word)[0] if not dictionary.check(word) and dictionary.suggest(word) else word for word in msg.split()])
     return corrected_words
+=======
+                return random.choice(possible_responses)    
+
+    define = get_definition(msg);
+    if define != "none":
+        return define
+      
+    return random.choice(default_responses)
+
+def clean_tokens(tokens):
+    stop_words = set(stopwords.words('english'))
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(token.lower()) for token in tokens if token.lower() not in stop_words and token not in string.punctuation]
+
+# get the definition of words that is not available in the intents.json
+def get_definition(input_msg): # using WordNet
+    # get the word from the input
+    cleaned_tokens = clean_tokens(nltk.word_tokenize(input_msg))
+    if cleaned_tokens:
+        word = cleaned_tokens[0]
+        # get definition
+        synsets = wordnet.synsets(word)
+        if synsets:
+            definition = synsets[0].definition()
+            return f"The word {word} means " + definition + "."
+    return f"none"
+
+# code if the user wants to know the POS of a word (not yet done here) 
+def get_POS(word, pos):
+    tag_map = {"NOUN": "n", "VERB": "v", "ADJ": "a", "ADV": "r", "ADP": "r", "CONJ": "c", "DET": "d", "PRT": "p"}
+    pos = tag_map.get(pos.upper())
+
+    # check if the given POS is valid
+    if not pos:
+        return False
+    
+    synsets = wordnet.synsets(word, pos=pos)
+    if synsets: return "yes"
+    else: return "no"
+
+def spell(match):
+    word = match.group(0)
+    return dictionary.suggest(word)[0] if not dictionary.check(word) and dictionary.suggest(word) else word
+>>>>>>> Stashed changes
 
 flag = False
 text = []
@@ -107,6 +178,7 @@ def chatbot_response(input_msg):
     global text
     global flag
     global context
+<<<<<<< Updated upstream
     input_msg = input_msg.lower()
     input_msg = re.sub(r'[^\w\s]', '', input_msg)
     correct_msg = spellcheck(input_msg)
@@ -139,6 +211,50 @@ def chatbot_response(input_msg):
             flag = True
 
 
+=======
+    global default_responses
+
+    res = ["Can you please clarify what you're asking or provide more information so I can better assist you?",
+           "Okay. Could you please clarify your question so I can assist you better?",
+           "Okay. Could you please rephrase your question or provide more details so I can better understand how to help you?"]
+
+    input_msg = input_msg.lower()
+
+    correct_msg = re.sub(r"\w+", spell, input_msg) # correct the spelling of the input_msg
+
+    if flag:
+        output_word=[correct_msg for correct_msg in text]
+        output_txt=" ".join(output_word)
+        print("current input: ", output_txt)
+
+        if correct_msg == "yes" or correct_msg in output_txt:
+            ints = predict_class(output_txt, model)
+            res = getResponse(ints, intents, output_txt)
+            print(ints)
+            text.clear()
+            flag = False
+        elif correct_msg == "no":
+            res = random.choice(res)
+            flag = False
+        else:
+            res = random.choice(default_responses)
+            flag = False
+    else:
+        if correct_msg == input_msg: # the input is correctly spelled
+            ints = predict_class(input_msg, model)
+            print(correct_msg, ints)
+            res = getResponse(ints, intents, correct_msg)
+            print("Previous Context:", previous_context, " Current Context: ", current_context)
+
+            text.clear()
+            flag = False
+        else:
+            text.append(correct_msg)
+            res = f"Sorry, Did you mean \"{correct_msg}\" instead of \"{input_msg}\"? Please enter yes or no."
+            flag = True
+
+
+>>>>>>> Stashed changes
         if any(context in current_context for context in previous_context):
             print("The context is similar")
         else:
